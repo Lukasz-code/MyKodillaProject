7dc11c33098c5ffc6a503b65a51c8b87c762650d
unknown <lukaszschweda@gmail.com>
2020-05-01 20:51:14 +0200

    zadanie 19.2

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -2,11 +2,28 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Getter
+import javax.persistence.*;
+
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Entity(name ="tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="ID")
     private Long id;
+
+    @Column(name="name")
     private String title;
+
+    @Column(name ="description")
     private String content;
+
+
 }
